8ebcfbf5b45c91efcf531b680ed6096ec11210fd
Tyler Palko <88953270+TylerPalko@users.noreply.github.com>
2023-01-20 17:37:31 -0500

    Add Snow Rider 3D

---
+++ snowrider3d/dictionary.bin.js
@@ -0,0 +1,2 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<Error><Code>AccessDenied</Code><Message>Access Denied</Message><RequestId>9Y2HJZ07CFGJTXTP</RequestId><HostId>3YhuomlTsPjlR8ZkZ82Atk9881UxzIZJ7TwdD897/6tjMy8VHTBLunFhRAKBmMXkz/GvV8NMrgo=</HostId></Error>
