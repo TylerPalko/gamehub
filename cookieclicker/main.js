9421c01a15172737e56e6f8fc1999011da08169c
Tyler Palko <88953270+TylerPalko@users.noreply.github.com>
2023-01-20 09:57:08 -0500

    Previous fix didint work so im just adding the code back so theres no confusion

--- cookieclicker/main.js
+++ cookieclicker/main.js
@@ -1883,6 +1883,14 @@ Game.Launch=function()
 		else Game.Loader.loaded=callback;
 		Game.Loader.Load(['filler.png']);
 	}
+	Game.ErrorFrame=function()
+	{
+		l('offGameMessage').innerHTML=
+		'<div class="title">Oops. Wrong address!</div>'+
+		'<div>It looks like you\'re accessing Cookie Clicker from another URL than the official one.<br>'+
+		'You can <a href="//orteil.dashnet.org/cookieclicker/" target="_blank">play Cookie Clicker over here</a>!<br>'+
+		'<small>(If for any reason, you are unable to access the game on the official URL, we are currently working on a second domain.)</small></div>';
+	}
 	Game.timedout=false;
 	Game.Timeout=function()
 	{
