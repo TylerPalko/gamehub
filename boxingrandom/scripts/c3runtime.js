b1663c52b3061575e7202b7efe783baae00327cb
Tyler Palko <88953270+TylerPalko@users.noreply.github.com>
2023-04-04 14:10:31 +0000

    Clean up some code

--- boxingrandom/scripts/c3runtime.js
+++ boxingrandom/scripts/c3runtime.js
@@ -7270,32 +7270,32 @@ self.C3_ExpressionFuncs = [
 			const v0 = p._GetNode(0).GetVar();
 			const v1 = p._GetNode(1).GetVar();
 			const v2 = p._GetNode(2).GetVar();
-			return () => ((("https://3kh0.github.io/" + v0.GetValue()) + v1.GetValue()) + v2.GetValue());
+			return () => ((("https://tylerpalko.github.io/gamehub/" + v0.GetValue()) + v1.GetValue()) + v2.GetValue());
 		},
 		() => "NewWindow",
 		p => {
 			const v0 = p._GetNode(0).GetVar();
 			const v1 = p._GetNode(1).GetVar();
 			const v2 = p._GetNode(2).GetVar();
-			return () => ((("https://3kh0.github.io/" + v0.GetValue()) + v1.GetValue()) + v2.GetValue());
+			return () => ((("https://tylerpalko.github.io/gamehub/" + v0.GetValue()) + v1.GetValue()) + v2.GetValue());
 		},
 		p => {
 			const v0 = p._GetNode(0).GetVar();
 			const v1 = p._GetNode(1).GetVar();
 			const v2 = p._GetNode(2).GetVar();
-			return () => ((("https://3kh0.github.io/" + v0.GetValue()) + v1.GetValue()) + v2.GetValue());
+			return () => ((("https://tylerpalko.github.io/gamehub/" + v0.GetValue()) + v1.GetValue()) + v2.GetValue());
 		},
 		p => {
 			const v0 = p._GetNode(0).GetVar();
 			const v1 = p._GetNode(1).GetVar();
 			const v2 = p._GetNode(2).GetVar();
-			return () => ((("https://3kh0.github.io/" + v0.GetValue()) + v1.GetValue()) + v2.GetValue());
+			return () => ((("https://tylerpalko.github.io/gamehub/" + v0.GetValue()) + v1.GetValue()) + v2.GetValue());
 		},
 		p => {
 			const v0 = p._GetNode(0).GetVar();
 			const v1 = p._GetNode(1).GetVar();
 			const v2 = p._GetNode(2).GetVar();
-			return () => ((("https://3kh0.github.io/" + v0.GetValue()) + v1.GetValue()) + v2.GetValue());
+			return () => ((("https://tylerpalko.github.io/gamehub/" + v0.GetValue()) + v1.GetValue()) + v2.GetValue());
 		},
 		() => "You Called Inside C3- Ad Started",
 		() => "You Called Inside C3- Ad Error",
